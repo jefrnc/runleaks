dd833aeb7b412b18e91b1a651b53e27d13a502b7
Jose Franco <josfranco@Joses-MBP.fibertel.com.ar>
2022-09-07 19:30:00 -0300

    test

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:jammy
 RUN apt-get update -y
 RUN apt-get install git gh make parallel jq -y
 
-RUN git clone https://github.com/JosiahSiegel/git-secrets.git
+RUN git clone https://github.com/msalemcode/git-secrets.git
 RUN make -C /git-secrets install
 COPY lib/* /
 
--- lib/scan.sh
+++ lib/scan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git config --global --add safe.directory /github/workspace
 
 # Init
 repo="$2"
@@ -51,6 +50,7 @@ if [[ $? -ne 0 ]]; then
   exit 1
 fi
 
+git config --global --add safe.directory /github/workspace
 echo "Repo: $repo"
 echo "============================================================================================"
 echo "Scan patterns:"
@@ -67,61 +67,59 @@ run_list_limit=$(($max_days_old * 400))
 
 echo "Raw run list limited to: $run_list_limit"
 
-# Collect ids of workflow runs
-declare -a run_ids=$(gh run list --repo "$repo" --json databaseId,status,updatedAt --jq '.[] | select(.updatedAt <= ((now - ('"$min_days_old"'*86400))|strftime("%Y-%m-%dT%H:%M:%S %Z"))) | select(.updatedAt > ((now - ('"$max_days_old"'*86400))|strftime("%Y-%m-%dT%H:%M:%S %Z"))) | select(.status =="completed").databaseId' --limit $run_list_limit)
-run_ids_limited=$(printf "%s\n" ${run_ids[@]} | head -$run_count)
 
-echo "Runs to scan: $run_count"
-echo $run_ids_limited
+git_hash=$(git rev-parse --short "$GITHUB_SHA")
+git_branch=${GITHUB_REF#refs/heads/}
+
+#echo "Runs to scan: $run_count"
+#echo $run_ids_limited
 
 touch "$log_file"
 
-run_for_each() {
-  local each=$@
+
 
   # Collect run logs and remove null
-  log_out=$(gh run view $each --repo "$repo" --log 2>/dev/null | sed 's/\x0//g')
-  if [[ $? -ne 0 ]]; then
-    exit 1
-  fi
+  #log_out=$(gh run view $each --repo "$repo" --log 2>/dev/null | sed 's/\x0//g')
+  #if [[ $? -ne 0 ]]; then
+  #  exit 1
+  #fi
 
-  echo "log_out -| $log_out"
+  #echo "log_out -| $log_out"
   # Identify potential exceptions
-  scan_out=$(echo "$log_out" | git secrets --scan - 2>&1)
+  scan_out=$(git secrets --scan - 2>&1)
   status=$?
 
   # If exception, add to array of details
   if (($status != 0)); then
     raw_log_full=$(echo "$scan_out" | grep '(standard input)')
     
-    echo "scan_out -| $scan_out"
-    echo "raw_log_full -| $raw_log_full"
+    #echo "scan_out -| $scan_out"
+    #echo "raw_log_full -| $raw_log_full"
     
     exception_line=$(echo "$raw_log_full" | awk -F '\t' '{print $1$2}' | sed 's/[^a-zA-Z0-9]/_/g' | sed 's/.*/"&",/')
     
-    echo "exception_line -| $exception_line"
+    #echo "exception_line -| $exception_line"
     
      
     exception=$(gh run view $each --repo "$repo" --json name,createdAt,databaseId,url,updatedAt,headBranch 2>/dev/null)
-    echo "exception -| $exception"
+    #echo "exception -| $exception"
     
     if [[ $? -ne 0 ]]; then
       exit 1
     fi
     exception_with_detail=$(echo $exception | jq '. + {exception_detail: {'"$exception_line"'}}')
-    echo $exception_with_detail >>"$log_file"
+    #echo $exception_with_detail >>"$log_file"
   fi
-}
+
 
 # Make visible to subprocesses
 export -f run_for_each
 export log_file
 export repo
 
-parallel -0 --jobs $max_proc_count run_for_each ::: $run_ids_limited
-echo "--------------------"
-cat $log_file
-echo "--------------------"
+#echo "--------------------"
+#cat $log_file
+#echo "--------------------"
 json_out=$(jq -n '.exceptions |= [inputs]' "$log_file")
 json_out_length=$(echo $json_out | jq -s '.[].exceptions' | jq length)
 echo "$json_out"
@@ -133,9 +131,11 @@ json_out="${json_out//$'\r'/'%0D'}"
 echo "::set-output name=exceptions::$json_out"
 echo "::set-output name=count::$json_out_length"
 
-rm "$log_file"
+#rm "$log_file"
 
 if [[ $fail_on_leak = true && json_out_length -gt 0 ]]; then
   echo "Failing since leak!"
   exit 1
 fi
+
+#sleep 5m # Waits 5 minutes.
